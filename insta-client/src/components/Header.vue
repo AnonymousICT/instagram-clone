682ab827ff6f010169c308f1b8222393f446947f
AnonymousICT <kuohoward@gmail.com>
2020-06-29 16:59:09 -0700

    linked material icons into index.html

--- insta-client/src/components/Header.vue
+++ insta-client/src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="header">
     <h3 class="title">INSTA<span>CLONE</span></h3>
     <button class="logout-btn" @click="$store.commit('logout')">
-      <i class="material-icons">power_off</i> logout
+      <i class="material-icons">exit_to_app</i>
     </button>
   </header>
 </template>
